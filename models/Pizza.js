8273b8816d914fff813e0e565bbe88d0cc5f4198
besharpe5 <besharpe15@gmail.com>
2022-04-11 19:03:18 -0400

    added GET and POST functionality

---
+++ models/Pizza.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+// this creates the Pizza model using the PizzSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// this exports the Pizza model
+module.exports = Pizza;
